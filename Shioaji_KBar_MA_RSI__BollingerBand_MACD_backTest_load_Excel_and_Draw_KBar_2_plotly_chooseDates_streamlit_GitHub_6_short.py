4e122cac30951eb1d4dde55db06795bc86be213a
chill2003 <168526062+chill2003@users.noreply.github.com>
2024-06-10 15:35:52 +0800

    Update Shioaji_KBar_MA_RSI__BollingerBand_MACD_backTest_load_Excel_and_Draw_KBar_2_plotly_chooseDates_streamlit_GitHub_6_short.py

--- Shioaji_KBar_MA_RSI__BollingerBand_MACD_backTest_load_Excel_and_Draw_KBar_2_plotly_chooseDates_streamlit_GitHub_6_short.py
+++ Shioaji_KBar_MA_RSI__BollingerBand_MACD_backTest_load_Excel_and_Draw_KBar_2_plotly_chooseDates_streamlit_GitHub_6_short.py
@@ -32,7 +32,7 @@ stc.html(html_temp)
 # df_original.to_pickle('kbars_2330_2022-01-01-2022-11-18.pkl')
 
 ## 读取Pickle文件
-df_original = pd.read_pickle('kbars_2330_2022-01-01-2022-11-18.pkl')
+df_original = pd.read_pickle('2201.pkl')
 
 
 #df.columns  ## Index(['Unnamed: 0', 'time', 'open', 'low', 'high', 'close', 'volume','amount'], dtype='object')
