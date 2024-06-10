672ce113b06495fd1daa4760fbfc07d8a2ee0073
chill2003 <168526062+chill2003@users.noreply.github.com>
2024-06-10 14:22:18 +0800

    Update Shioaji_KBar_MA_RSI__BollingerBand_MACD_backTest_load_Excel_and_Draw_KBar_2_plotly_chooseDates_streamlit_GitHub_6_short.py

--- Shioaji_KBar_MA_RSI__BollingerBand_MACD_backTest_load_Excel_and_Draw_KBar_2_plotly_chooseDates_streamlit_GitHub_6_short.py
+++ Shioaji_KBar_MA_RSI__BollingerBand_MACD_backTest_load_Excel_and_Draw_KBar_2_plotly_chooseDates_streamlit_GitHub_6_short.py
@@ -49,12 +49,21 @@ df_original = df_original.drop('Unnamed: 0',axis=1)
 ##### 選擇資料區間
 #st.set_page_config(layout = 'wide',initial_sidebar_state='expanded')
 st.sidebar.header("選擇開始與結束的日期, 區間:2022-01-03 至 2022-11-18")
-start_date = st.sidebar.text_input('選擇開始日期 (日期格式: 2022-01-03)', '2022-01-03')
-end_date = st.sidebar.text_input('選擇結束日期 (日期格式: 2022-11-18)', '2022-11-18')
-start_date = datetime.datetime.strptime(start_date,'%Y-%m-%d')
-end_date = datetime.datetime.strptime(end_date,'%Y-%m-%d')
-# 使用条件筛选选择时间区间的数据
-df = df_original[(df_original['time'] >= start_date) & (df_original['time'] <= end_date)]
+import datetime
+
+# 用户输入的字符串日期
+start_date_str = st.sidebar.text_input('選擇開始日期 (日期格式: 2022-01-03)', '2022-01-03')
+end_date_str = st.sidebar.text_input('選擇結束日期 (日期格式: 2022-11-18)', '2022-11-18')
+
+# 将字符串日期转换为 datetime.datetime 对象
+start_date = datetime.datetime.strptime(start_date_str, '%Y-%m-%d')
+end_date = datetime.datetime.strptime(end_date_str, '%Y-%m-%d')
+
+# 确保比较的对象都是 datetime.datetime 类型
+if start_date <= end_date:
+    st.write("Start date is before or equal to end date.")
+else:
+    st.write("Start date is after end date.")
 
 
 ###### (2) 轉化為字典 ######:
